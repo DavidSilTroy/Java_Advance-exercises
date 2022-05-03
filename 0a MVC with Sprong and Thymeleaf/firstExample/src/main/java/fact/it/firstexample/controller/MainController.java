578bfb989ea1681ef4571e1d8052f81419c9dec4
Davidsiltroy <davidsiltroy2008@hotmail.com>
2022-05-03 14:13:28 +0200

    Learning more about the MVC of Spring Boot and Thymeleaf

---
+++ 0a MVC with Sprong and Thymeleaf/firstExample/src/main/java/fact/it/firstexample/controller/MainController.java
@@ -0,0 +1,16 @@
+package fact.it.firstexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController
+{
+    @RequestMapping("/")
+    public String studentInfo(Model model)
+    {
+        model.addAttribute("firstName", "David");
+        return "studentInfo";
+    }
+}
---
+++ 0a MVC with Sprong and Thymeleaf/firstExample/src/main/java/fact/it/firstexample/model/Model.java
@@ -0,0 +1,4 @@
+package fact.it.firstexample.model;
+
+public class Model {
+}
