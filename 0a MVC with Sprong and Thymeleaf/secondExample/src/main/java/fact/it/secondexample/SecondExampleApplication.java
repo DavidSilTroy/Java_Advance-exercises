22d67d072543b2956fff4b7a7daf25aff3384cdb
Davidsiltroy <davidsiltroy2008@hotmail.com>
2022-05-03 14:26:37 +0200

    Example 2 of the MVC using the model to create list and more views

---
+++ 0a MVC with Sprong and Thymeleaf/secondExample/src/main/java/fact/it/secondexample/SecondExampleApplication.java
@@ -0,0 +1,13 @@
+package fact.it.secondexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondExampleApplication.class, args);
+    }
+
+}
