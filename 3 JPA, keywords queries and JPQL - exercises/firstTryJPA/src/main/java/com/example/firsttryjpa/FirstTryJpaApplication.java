7e7470689393fb79b4b390a01eec8eae9790bea0
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-15 09:54:19 +0200

    Creating a new project to try the JPA

---
+++ 3 JPA, keywords queries and JPQL - exercises/firstTryJPA/src/main/java/com/example/firsttryjpa/FirstTryJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.firsttryjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTryJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstTryJpaApplication.class, args);
+    }
+
+}
